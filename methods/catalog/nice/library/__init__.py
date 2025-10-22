107ebaa7a1541a3eb0247ae886f4dbbcce834476
Jamie001129 <yujieming@outlook.com>
2025-10-22 15:58:24 -0400

    remove nice

--- methods/catalog/nice/library/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-from .data import data_NICE
-from .distance import HEOM, MinMaxDistance, StandardDistance, NearestNeighbour
-from .heuristic import best_first
-from .reward import SparsityReward, ProximityReward, PlausibilityReward
-from .autoencoder import AutoEncoder
